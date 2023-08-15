6954b9bbc0ccdc3ddf4f5580dccb6d46755fc38e
Tharindu Rukshan <tharindurukshan709@gmail.com>
2023-08-15 22:22:26 +0530

    LayoutView and CustomerView Developed

--- src/supermarket/pos/layerd/SupermarketPosLayerd.java
+++ src/supermarket/pos/layerd/SupermarketPosLayerd.java
@@ -4,6 +4,8 @@
  */
 package supermarket.pos.layerd;
 
+import supermarket.pos.layerd.view.LayoutView;
+
 /**
  *
  * @author thari
@@ -14,7 +16,7 @@ public class SupermarketPosLayerd {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello");
+        new LayoutView().setVisible(true);
     }
     
 }
